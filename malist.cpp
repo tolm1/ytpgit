b0c31a6c6ef16db8a0670bffc6dbcbfa8c5410f3
tolm1 <tolm6835@gmail.com>
2023-11-30 12:38:15 +0300

    malist.cpp add

---
+++ malist.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int max_in_list(int* a,int n) {
+    int ma = -100000000;
+    for (int i = 0; i < n; i++) {
+        if (a[i] > ma) {
+            ma = a[i];
+        }
+    }
+
+    return ma;
+}
+
+int mait(){
+    return 0;
+}
